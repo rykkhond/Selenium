795121451e37caff2e74edcf035c9ec3327a666c
2168382 <2168382@LTIN381433.cts.com>
2022-04-29 16:44:17 +0530

    First commit..

---
+++ src/Day1/Logintest.java
@@ -0,0 +1,24 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Logintest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","C://Users//2168382//Desktop/chromedriver.exe");
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	WebElement usernametxt	=driver.findElement(By.id("txtUsername"));
+	usernametxt.sendKeys("Admin");
+	 WebElement passwordtxt	=driver.findElement(By.id("txtPassword"));
+	 passwordtxt.sendKeys("admin123");
+	
+		driver.findElement(By.id("btnLogin")).click();
+	}
+
+}
